5330cf1d0645983167960e555931f22739439fc8
svalls26 <74300901+svalls26@users.noreply.github.com>
2021-11-09 10:01:02 +0100

    IOT

---
+++ Prueba_git/HolaMundo.py
@@ -0,0 +1,9 @@
+# Autor: Jorge Capel
+# Descripcion : Ejemplo de programa python para imprimir hola mundo en pantalla
+def sumar(a,b):
+    return a +b 
+resultado = sumar(5,6)
+print(resultado)
+def imprimir(texto):
+  print(texto);
+imprimir("Hola Mundo");
